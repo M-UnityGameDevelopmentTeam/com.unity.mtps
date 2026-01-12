68ab93b6f2a7d5ccc646bbe448941fc92b77a784
M-Unity <mihailsheshukov10@gmail.com>
2026-01-12 18:51:19 +0500

    A space for names

---
+++ MTPS/Runtime/TextPackList.cs
@@ -0,0 +1,33 @@
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace MUnity.MTPS
+{
+    public class TextPackList : MonoBehaviour
+    {
+        [SerializeField]
+        private GameObject Panel;
+
+        [SerializeField]
+        private GameObject ButtonPrefab;
+        private GameObject TempButton;
+        private int tempindex = -1;
+
+        private void Start()
+        {
+            foreach (TextPack textPack in FindFirstObjectByType<TextPackDirector>().FTextPacks)
+            {
+                tempindex += 1;
+                TempButton = Instantiate(ButtonPrefab, Panel.transform);
+                TempButton.GetComponentInChildren<TMP_Text>().text = textPack.Name;
+                var a = tempindex;
+                TempButton
+                    .GetComponent<Button>()
+                    .onClick.AddListener(() =>
+                        FindFirstObjectByType<TextPackDirector>().ChangeTextPack(a)
+                    );
+            }
+        }
+    }
+}
---
+++ MTPS/Runtime/TextPackTMPClient.cs
@@ -0,0 +1,22 @@
+using TMPro;
+using UnityEngine;
+
+namespace MUnity.MTPS
+{
+    public class TextPackTMPClient : MonoBehaviour
+    {
+        private TextPackDirector Extracter;
+        private TMP_Text ClientText;
+        public string Key;
+
+        private void Start()
+        {
+            ClientText = GetComponent<TMP_Text>();
+            Extracter = FindFirstObjectByType<TextPackDirector>();
+            Key = ClientText.text;
+            UpdateText();
+        }
+
+        public void UpdateText() => ClientText.text = Extracter.FCurrentTextPack[Key];
+    }
+}
