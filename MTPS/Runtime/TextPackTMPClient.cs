ba0614c401f9bb93acbec995f0c7088ada321ab2
M-Unity <mihailsheshukov10@gmail.com>
2026-02-16 17:37:33 +0500

    fix

--- MTPS/Runtime/TextPackTMPClient.cs
+++ MTPS/Runtime/TextPackTMPClient.cs
@@ -18,6 +18,7 @@ namespace MTPS
 
         private void Awake()
         {
+            GetKeys();
             textDirector = FindFirstObjectByType<TextPackDirector>();
             TextPackUpdate();
         }
